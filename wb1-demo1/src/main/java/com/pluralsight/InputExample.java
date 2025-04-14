5e65b6345b7501d29a70372db85811c32067d7ab
arthurmov <officialamovsesyan@gmail.com>
2025-04-14 15:36:09 -0400

    in class work - scanners

---
+++ wb1-demo1/src/main/java/com/pluralsight/InputExample.java
@@ -0,0 +1,22 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class InputExample {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is the first number?");
+        double number1 = scanner.nextDouble();
+
+        System.out.println("What is the second number?");
+        double number2 = scanner.nextDouble();
+
+        double product = number1 * number2;
+
+        System.out.printf("The product of %.1f and %.1f is %.1f \n", number1, number2, product);
+
+        // System.out.println("The product of " + number1 + " and " + number2 + " is " + product);
+    }
+}
