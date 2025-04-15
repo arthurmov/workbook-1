5b909e13a24bc4a2c5538326f6cc12b3aa9e57bf
arthurmov <officialamovsesyan@gmail.com>
2025-04-15 10:25:04 -0400

    Added comments

--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalc.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalc.java
@@ -7,19 +7,25 @@ public class PayrollCalc {
 
         Scanner scanner = new Scanner(System.in);
 
+        //Get the known values
         System.out.println("What is your name?");
         String name = scanner.next();
+
         System.out.println("How many hours did you work?");
         float hours = scanner.nextFloat();
+
         System.out.println("What is your pay rate?");
         float rate = scanner.nextFloat();
 
+        //Check if the employee worked overtime, then print the results
         if ( hours > 40) {
             float overtimeHours = hours - 40;
             double overtimePay = (overtimeHours * rate) * 1.5;
             double pay = (rate * hours) + overtimePay;
             System.out.println(name + " generated a gross pay of $" + pay);
-        } else {
+        }
+
+        else {
             double pay = rate * hours;
             System.out.println(name + " generated a gross pay of $" + pay);
         }
