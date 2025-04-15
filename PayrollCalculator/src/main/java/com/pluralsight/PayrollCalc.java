7b79cdfdd27d9389596ffcd00a54416382092e1f
arthurmov <officialamovsesyan@gmail.com>
2025-04-15 15:01:06 -0400

    Completed Mod3 - Exercise 3 - Basic Calculator

--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalc.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalc.java
@@ -3,33 +3,51 @@ package com.pluralsight;
 import java.util.Scanner;
 
 public class PayrollCalc {
+
+    //Initializes a static scanner
+    static Scanner scanner = new Scanner(System.in);
+    
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        //Initializes variables to methods that return values
+        String name = promptForName();
+        float hoursWorked = promptForHoursWorked();
+        double payRate = promptForPayRate();
 
-        //Get the known values
-        System.out.println("Please enter the employee name: ");
-        String name = scanner.next();
+        double grossPay = calcGrossPay(hoursWorked, payRate);
 
-        System.out.println("Please enter the number of hours worked: ");
-        float hours = scanner.nextFloat();
+        System.out.printf("%s worked %.0f hours at a pay rate of %.0f, and generated a gross pay of %.2f", name, hoursWorked, payRate, grossPay);
+    }
 
-        System.out.println("Please enter the pay rate: ");
-        float rate = scanner.nextFloat();
-
-        double pay;
-
-        //Check if the employee worked overtime, then print the results
-        if (hours > 40) {
-            float overtimeHours = hours - 40;
-            double basePay = 40 * rate;
-            double overtimePay = overtimeHours * rate * 1.5;
-            pay = basePay + overtimePay;
+    public static double calcGrossPay(float hoursWorked, double payRate) {
+        //Checks for overtime hours, then returns gross pay
+        if (hoursWorked > 40) {
+            float overtimeHours = hoursWorked - 40;
+            double basePay = 40 * payRate;
+            double overtimePay = overtimeHours * payRate * 1.5;
+            return basePay + overtimePay;
         }
-        //Prints the employees pay if they didn't work overtime
+        //Returns gross pay if no overtime
         else {
-            pay = rate * hours;
+            return payRate * hoursWorked;
         }
-        System.out.printf("%s generated a gross pay of $%.2f", name, pay);
+    }
+
+    //Method to get name
+    public static String promptForName() {
+        System.out.println("Please enter the employee name: ");
+        return scanner.next();
+    }
+
+    //Method to get hours worked
+    public static float promptForHoursWorked() {
+        System.out.println("Please enter how many hours worked: ");
+        return scanner.nextFloat();
+    }
+
+    //Method to get pay rate
+    public static double promptForPayRate() {
+        System.out.println("Please enter your pay rate: ");
+        return scanner.nextDouble();
     }
 }
